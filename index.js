e97192ad8bc6df7eb02c84ee503292140276a45d
hasanonik <jh8539615@gmail.com>
2024-04-30 10:06:03 +0600

    deleted unnecessary code

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jahid12.81vfswo.mongodb.net/?retryWrites=true&w=majority&appName=jahid12`;
 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -25,8 +24,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
 
         const tourSpotsCollection = client.db("tourismDB").collection("touristsSpot")
         const countriesCollection = client.db("tourismDB").collection("countries")
@@ -122,10 +119,6 @@ async function run() {
             const result = await tourSpotsCollection.deleteOne(query);
             res.send(result)
         })
-
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally {
         // Ensures that the client will close when you finish/error
